798badb66d05df260ee576028bafcbc804989805
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2022-09-20 17:16:36 +0700

    done

--- hit_faceTracking.py
+++ hit_faceTracking.py
@@ -8,20 +8,40 @@ import threading
 import argparse
 import math
 import os
+
+
 def def_value(): 
     return "_"
 
 # app = Flask(__name__)
-retina_face = RetinaFace(model_name='retina_m')
-arc_face = ArcFace(model_name='arcface_m')
+retina_face = RetinaFace(model_name='retina_s')
+arc_face = ArcFace(model_name='arcface_s')
 bt = BYTETracker()
 landmark = Landmark()
-gamma = 37.7952755906
+
+
 recog_data = {
     'userID':[],
     'emb':[],
     'trackID':[]
 }
+database_emb = {
+    'userID': [],
+    'embs': []
+}
+img_data_list = os.listdir('data')
+for i in range(len(img_data_list)):
+    img_path = os.path.join('data', img_data_list[i])
+    img = cv2.imread(img_path)
+    fbox, kpss = retina_face.detect(img)
+    tbox, tids = bt.predict(img, fbox)
+    print(kpss[0])
+    # face = img[box[1]:box[3],box[0]:box[2]]    
+    emb = arc_face.get(img, kpss[0])
+    database_emb['embs'].append(emb)
+    database_emb['userID'].append(img_data_list[i][:-4])
+print('Extract feature on databse done!')
+
 track_name = defaultdict(def_value)
 track_emb = {}
 current_tracking = {}
@@ -70,7 +90,23 @@ def recog():
         
         current_tracking = {'frame':frame.copy(), 'track_id': ids, 'embs':embs} 
         
-        print(len(current_tracking['embs']))
+        for idt, emb, tbox in zip(current_tracking['track_id'], current_tracking['embs'], tboxes):
+            box = tbox[:4].astype(int)
+            
+            dis = np.linalg.norm(database_emb['embs'] - emb, axis = 1)
+            if (min(dis) < 25):
+                idx = np.argmin(dis)
+                t = time.time()
+                print(database_emb['userID'][idx])
+                # if (time.time() - t > 5):
+                cv2.putText(frame, str(database_emb['userID'][idx]), (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0),2)
+            else:
+                t = time.time()
+                print("Stranger")
+                # if (time.time() - t > 5):
+                cv2.putText(frame, "Stranger", (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0),2)
+                    
+        # print(len(current_tracking['embs']))
         # print(current_tracking)
         cv2.imshow('qwe', frame)
         cv2.waitKey(1)
@@ -98,6 +134,8 @@ def draw_fancy_box(img, pt1, pt2, color, thickness, r, d):
     cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
     cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
     cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
+
+    
         
 if __name__ == '__main__':
     recog()
